998dc33c5a6b691935e55c8f1563b1b0edaf2c09
Elvis Espinoza <elvis.espinoza.navarrete@outlook.com>
2023-08-28 19:09:22 -0400

    wip: set up bakcend files

---
+++ backend/tattoo-portfolio-backend/src/main/java/io/github/elvisEspinozaN/tattooportfoliobackend/TattooPortfolioBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.elvisEspinozaN.tattooportfoliobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TattooPortfolioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TattooPortfolioBackendApplication.class, args);
+	}
+
+}
---
+++ backend/tattoo-portfolio-backend/src/test/java/io/github/elvisEspinozaN/tattooportfoliobackend/TattooPortfolioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.elvisEspinozaN.tattooportfoliobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TattooPortfolioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
